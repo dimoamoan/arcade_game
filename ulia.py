38f4286f915d4a1a38f2490249aec556701fd8e1
Юлька <yulianizhenko@yandex.ru>
2026-01-29 20:56:13 +0400

    уровень сделанный

--- ulia.py
+++ ulia.py
@@ -1,97 +1,92 @@
 import arcade
 import random
-
-# Константы
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
-SCREEN_TITLE = "Space Survival: Win or Lose"
-
-PLAYER_SPEED = 5
-BULLET_SPEED = 7
-ALIEN_SPEED = 3
-RESOURCE_SPEED = 2
-
-# Состояния игры
-STATE_PLAYING = 0
-STATE_WON = 1
-STATE_LOST = 2
+import constants as c  # Импортируем наш файл с константами
 
 
 class MyGame(arcade.Window):
     def __init__(self):
-        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+        super().__init__(c.SCREEN_WIDTH, c.SCREEN_HEIGHT, c.SCREEN_TITLE)
 
-        # Списки спрайтов
         self.player_list = None
         self.alien_list = None
         self.resource_list = None
         self.bullet_list = None
 
+        # Переменные для фона и звуков
+        self.background_texture = None
+        self.sound_shoot = None
+        self.sound_collect = None
+        self.sound_hit = None
+
         self.player_sprite = None
         self.score = 0
         self.resources = 0
-        self.state = STATE_PLAYING
+        self.state = c.STATE_PLAYING
 
-        # Создаем текстуры программно
+        # Программные текстуры (оставляем, чтобы не грузить лишние файлы спрайтов)
         self.player_texture = arcade.make_circle_texture(40, arcade.color.CYAN)
         self.alien_texture = arcade.make_soft_circle_texture(30, arcade.color.RED_ORANGE)
         self.resource_texture = arcade.make_circle_texture(20, arcade.color.GOLD)
 
-        arcade.set_background_color(arcade.color.BLACK_OLIVE)
-
     def setup(self):
-        """ Сброс игры к начальному состоянию """
         self.player_list = arcade.SpriteList()
         self.alien_list = arcade.SpriteList()
         self.resource_list = arcade.SpriteList()
         self.bullet_list = arcade.SpriteList()
 
+        # Загрузка внешних ресурсов
+        try:
+            self.background_texture = arcade.load_texture(c.BACKGROUND_PATH)
+            self.sound_shoot = arcade.load_sound(c.SOUND_SHOOT)
+            self.sound_collect = arcade.load_sound(c.SOUND_COLLECT)
+            self.sound_hit = arcade.load_sound(c.SOUND_HIT)
+        except Exception as e:
+            print(f"Ошибка загрузки файлов: {e}. Проверьте наличие файлов в папке.")
+
         self.player_sprite = arcade.Sprite(self.player_texture)
-        self.player_sprite.center_x = SCREEN_WIDTH // 2
+        self.player_sprite.center_x = c.SCREEN_WIDTH // 2
         self.player_sprite.center_y = 100
         self.player_list.append(self.player_sprite)
 
         self.score = 0
         self.resources = 0
-        self.state = STATE_PLAYING
+        self.state = c.STATE_PLAYING
 
-        # Сброс таймеров (чтобы они не дублировались при перезапуске)
         arcade.unschedule(self.spawn_alien)
         arcade.unschedule(self.spawn_resource)
         arcade.schedule(self.spawn_alien, 1.0)
         arcade.schedule(self.spawn_resource, 2.5)
 
-    def draw_game_over(self, title, color):
-        """ Универсальный метод для рисования экранов финала """
-        arcade.draw_text(title, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 + 20,
-                         color, 50, anchor_x="center")
-        arcade.draw_text(f"Счет: {self.score} | Ресурсы: {self.resources}",
-                         SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 - 40,
-                         arcade.color.WHITE, 16, anchor_x="center")
-        arcade.draw_text("Нажмите 'R' для перезапуска или 'ESC' для выхода",
-                         SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 - 80,
-                         arcade.color.LIGHT_GRAY, 12, anchor_x="center")
-
     def on_draw(self):
         self.clear()
 
-        if self.state == STATE_PLAYING:
+        # Рисуем фон, если он загружен
+        if self.background_texture:
+            arcade.draw_lrwh_rectangle_textured(0, 0, c.SCREEN_WIDTH, c.SCREEN_HEIGHT, self.background_texture)
+        else:
+            arcade.set_background_color(arcade.color.BLACK_OLIVE)
+
+        if self.state == c.STATE_PLAYING:
             self.alien_list.draw()
             self.resource_list.draw()
             self.bullet_list.draw()
             self.player_list.draw()
 
-            arcade.draw_text(f"Счет: {self.score} / 100", 10, 570, arcade.color.WHITE, 12)
-            arcade.draw_text(f"Ресурсы: {self.resources} / 100", 10, 550, arcade.color.GOLD, 12)
+            arcade.draw_text(f"Счет: {self.score} / {c.WIN_SCORE}", 10, 570, arcade.color.WHITE, 12)
+            arcade.draw_text(f"Ресурсы: {self.resources} / {c.WIN_RESOURCES}", 10, 550, arcade.color.GOLD, 12)
 
-        elif self.state == STATE_WON:
-            self.draw_game_over("ПОБЕДА!", arcade.color.GOLD)
+        elif self.state == c.STATE_WON:
+            self.draw_end_screen("ПОБЕДА!", arcade.color.GOLD)
+        elif self.state == c.STATE_LOST:
+            self.draw_end_screen("ИГРА ОКОНЧЕНА", arcade.color.RED)
 
-        elif self.state == STATE_LOST:
-            self.draw_game_over("ИГРА ОКОНЧЕНА", arcade.color.RED)
+    def draw_end_screen(self, title, color):
+        arcade.draw_text(title, c.SCREEN_WIDTH / 2, c.SCREEN_HEIGHT / 2 + 20, color, 50, anchor_x="center")
+        arcade.draw_text("Нажмите 'R' для перезапуска", c.SCREEN_WIDTH / 2, c.SCREEN_HEIGHT / 2 - 40,
+                         arcade.color.WHITE, 16, anchor_x="center")
 
     def on_update(self, delta_time):
-        if self.state != STATE_PLAYING:
+        if self.state != c.STATE_PLAYING:
             return
 
         self.player_list.update()
@@ -99,61 +94,55 @@ class MyGame(arcade.Window):
         self.alien_list.update()
         self.resource_list.update()
 
-        # Ограничения экрана
-        if self.player_sprite.left < 0: self.player_sprite.left = 0
-        if self.player_sprite.right > SCREEN_WIDTH: self.player_sprite.right = SCREEN_WIDTH
-        if self.player_sprite.bottom < 0: self.player_sprite.bottom = 0
-        if self.player_sprite.top > SCREEN_HEIGHT: self.player_sprite.top = SCREEN_HEIGHT
-
         # Столкновения: пули и враги
         for bullet in self.bullet_list:
             hit_list = arcade.check_for_collision_with_list(bullet, self.alien_list)
             if hit_list:
                 bullet.remove_from_sprite_lists()
+                if self.sound_hit: arcade.play_sound(self.sound_hit)
                 for alien in hit_list:
                     alien.remove_from_sprite_lists()
                     self.score += 10
 
-        # Столкновения: игрок и враги (ШТРАФ)
-        if arcade.check_for_collision_with_list(self.player_sprite, self.alien_list):
-            for enemy in arcade.check_for_collision_with_list(self.player_sprite, self.alien_list):
+        # Столкновения: игрок и враги
+        enemy_hits = arcade.check_for_collision_with_list(self.player_sprite, self.alien_list)
+        if enemy_hits:
+            if self.sound_hit: arcade.play_sound(self.sound_hit)
+            for enemy in enemy_hits:
                 enemy.remove_from_sprite_lists()
                 self.score -= 10
 
         # Сбор ресурсов
-        res_list = arcade.check_for_collision_with_list(self.player_sprite, self.resource_list)
-        for res in res_list:
-            res.remove_from_sprite_lists()
-            self.resources += 10
+        res_hits = arcade.check_for_collision_with_list(self.player_sprite, self.resource_list)
+        if res_hits:
+            if self.sound_collect: arcade.play_sound(self.sound_collect)
+            for res in res_hits:
+                res.remove_from_sprite_lists()
+                self.resources += 10
 
-        # ПРОВЕРКА УСЛОВИЙ
         if self.score < 0:
-            self.state = STATE_LOST
-        elif self.score >= 100 and self.resources >= 100:
-            self.state = STATE_WON
-
-        # Чистка памяти
-        for alien in self.alien_list:
-            if alien.top < 0: alien.remove_from_sprite_lists()
+            self.state = c.STATE_LOST
+        elif self.score >= c.WIN_SCORE and self.resources >= c.WIN_RESOURCES:
+            self.state = c.STATE_WON
 
     def on_key_press(self, key, modifiers):
-        if self.state == STATE_PLAYING:
+        if self.state == c.STATE_PLAYING:
             if key == arcade.key.LEFT:
-                self.player_sprite.change_x = -PLAYER_SPEED
+                self.player_sprite.change_x = -c.PLAYER_SPEED
             elif key == arcade.key.RIGHT:
-                self.player_sprite.change_x = PLAYER_SPEED
+                self.player_sprite.change_x = c.PLAYER_SPEED
             elif key == arcade.key.UP:
-                self.player_sprite.change_y = PLAYER_SPEED
+                self.player_sprite.change_y = c.PLAYER_SPEED
             elif key == arcade.key.DOWN:
-                self.player_sprite.change_y = -PLAYER_SPEED
+                self.player_sprite.change_y = -c.PLAYER_SPEED
             elif key == arcade.key.SPACE:
+                if self.sound_shoot: arcade.play_sound(self.sound_shoot)
                 bullet = arcade.SpriteSolidColor(5, 15, arcade.color.WHITE)
                 bullet.center_x = self.player_sprite.center_x
                 bullet.bottom = self.player_sprite.top
-                bullet.change_y = BULLET_SPEED
+                bullet.change_y = c.BULLET_SPEED
                 self.bullet_list.append(bullet)
 
-        # Перезапуск
         if key == arcade.key.R:
             self.setup()
         elif key == arcade.key.ESCAPE:
@@ -164,19 +153,19 @@ class MyGame(arcade.Window):
         if key in (arcade.key.UP, arcade.key.DOWN): self.player_sprite.change_y = 0
 
     def spawn_alien(self, delta_time):
-        if self.state == STATE_PLAYING:
+        if self.state == c.STATE_PLAYING:
             alien = arcade.Sprite(self.alien_texture)
-            alien.center_x = random.randrange(SCREEN_WIDTH)
-            alien.top = SCREEN_HEIGHT
-            alien.change_y = -ALIEN_SPEED
+            alien.center_x = random.randrange(c.SCREEN_WIDTH)
+            alien.top = c.SCREEN_HEIGHT
+            alien.change_y = -c.ALIEN_SPEED
             self.alien_list.append(alien)
 
     def spawn_resource(self, delta_time):
-        if self.state == STATE_PLAYING:
+        if self.state == c.STATE_PLAYING:
             res = arcade.Sprite(self.resource_texture)
-            res.center_x = random.randrange(SCREEN_WIDTH)
-            res.top = SCREEN_HEIGHT
-            res.change_y = -RESOURCE_SPEED
+            res.center_x = random.randrange(c.SCREEN_WIDTH)
+            res.top = c.SCREEN_HEIGHT
+            res.change_y = -c.RESOURCE_SPEED
             self.resource_list.append(res)
 
 
